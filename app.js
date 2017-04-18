dc336e1be3b344f45ddc4ef9c0b2d6182de2908c
aripian <aripian.android@gmail.com>
2017-04-18 23:02:22 +0800

    Added index html

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
 var port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('This Works!')
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(port, function () {

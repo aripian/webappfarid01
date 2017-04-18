56bb5b1904d62a059c8393116d2dff5f54098bdb
aripian <aripian.android@gmail.com>
2017-04-18 22:36:02 +0800

    Change port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 3000
+
 app.get('/', function (req, res) {
   res.send('This Works!')
 })
 
-app.listen(3000, function () {
-  console.log('App is listening on port 3000!')
+app.listen(port, function () {
+  console.log('App is listening on port '+ port +'!')
 })

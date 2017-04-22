2b9863e03de162f4dff056c924b6f9982ea7ec20
aripian <aripian.android@gmail.com>
2017-04-22 08:22:27 +0800

    Constructed web skeleton

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 
 var port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })

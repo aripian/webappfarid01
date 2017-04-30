a82540e99082f8ff96da5f29600df02b9669edcb
Muhammad Nor Arif Zakaria <muhammad.norarif@axa.com.my>
2017-04-30 14:24:18 +0800

    Added email functionality

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express')
 var app = express()
 var path = require('path');
+var bodyParser = require('body-parser');
+
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'aripian.webapp@gmail.com',
+		pass: 'aripian123'
+	}
+})
 
 var port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,6 +27,24 @@ app.use('/scripts', express.static(__dirname + '/node_modules'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.post('/sendmail',function(req, res){
+	
+	let mailOptions = {
+	    from: 'aripian.webapp@gmail.com', // sender address
+	    to: req.body.data.email, // list of receivers
+	    subject: 'Terima kasih '+req.body.data.name+' menghubungi kami!', // plain text body
+	    html: '<b>Terima kasih '+req.body.data.name+' mengubungi kami!</b>' // html body
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+	    if (error) {
+	        res.json(error);
+	    }
+	    res.json(info);
+	});
+
 })
 
 app.listen(port, function () {
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var mainApp = angular.module("mainApp", ['angularModalService']);
          
-mainApp.controller('mainController', function($scope, ModalService) {
+mainApp.controller('mainController', function($scope, ModalService, $http) {
 
    $scope.contactUs = function() {
 
@@ -14,7 +14,16 @@ mainApp.controller('mainController', function($scope, ModalService) {
     }).then(function(modal) {
       modal.element.modal();
       modal.close.then(function(result) {
-        $scope.complexResult  = "Name: " + result.name + ", age: " + result.age;
+        if(result.user){
+          $http.post('/sendmail',{
+            data: result.user
+          }).then(function success(response) {
+              console.log(response)
+              swal("Terima Kasih "+result.user.name+"!", "Kami akan menghubungi anda semula dalam masa 7 hari", "success")
+          }, function myError(response) {
+              $scope.myWelcome = response.statusText;
+          });
+        }
       });
     });
 
@@ -24,16 +33,16 @@ mainApp.controller('mainController', function($scope, ModalService) {
 
 mainApp.controller('contactController', function($scope, $element, title, close) {
 
-  $scope.name = null;
-  $scope.age = null;
+  $scope.user = null;
   $scope.title = title;
+
+  $scope.state = ['Johor','Kedah','Kelantan','Melaka','Pahang','Perak','Perlis','Pulau Pinang','Sabah','Sarawak','Selangor','Terengganu','W.P Kuala Lumpur','W.P Labuan','W.P Putrajaya']
   
   //  This close function doesn't need to use jQuery or bootstrap, because
   //  the button has the 'data-dismiss' attribute.
   $scope.close = function() {
      close({
-      name: $scope.name,
-      age: $scope.age
+      user: $scope.user
     }, 500); // close, but give 500ms for bootstrap to animate
   };
 
@@ -46,8 +55,7 @@ mainApp.controller('contactController', function($scope, $element, title, close)
     
     //  Now call close, returning control to the caller.
     close({
-      name: $scope.name,
-      age: $scope.age
+      user: null
     }, 500); // close, but give 500ms for bootstrap to animate
   };
 
